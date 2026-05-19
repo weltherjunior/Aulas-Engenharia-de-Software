a871793831cb1b4e20f04ae73c0974c968e4b4f3
weltherjunior <weltherjunior@gmail.com>
2026-05-19 19:51:39 -0300

    atividade

---
+++ uploads/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Upload de Arquivo</title>
+</head>
+<body>
+
+<h2>Upload de Currículo</h2>
+
+
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  <label for="file">Escolha um arquivo:</label>
+  <input type="file" name="file" id="file" accept=".pdf,.doc,.docx"><br><br>
+  <input type="submit" value="Enviar">
+</form>
+
+<a href="uploads/6a0ce4aeb01c9_1779147345594.pdf">clique para ver o arquivo</a>
+
+</body>
+</html>
---
+++ uploads/upload.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$conn = new PDO("mysql:host=localhost;dbname=engsoft;charset=utf8mb4", "root", "");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $arquivo = $_FILES['file'];
+     
+
+    if($arquivo['error'] === UPLOAD_ERR_OK) {
+        $nomeArquivo = uniqid() . '_' . basename($arquivo['name']);
+        $destino = 'uploads/' . $nomeArquivo;
+         
+        if(move_uploaded_file($arquivo['tmp_name'], $destino)) {
+            echo "Arquivo enviado com sucesso: " . $nomeArquivo;
+            $stmt = $conn->prepare("INSERT INTO arquivos (nome_arquivo) VALUES (:nome_arquivo)");
+            $stmt->bindValue(':nome_arquivo', $nomeArquivo, PDO::PARAM_STR);
+            $stmt->execute();
+        } else {
+            echo "Erro ao salvar o arquivo.";
+        }
+    } else {
+        echo "Erro no upload: " . $arquivo['error'];
+    }
+} else {
+    echo "Nenhum arquivo enviado.";
+}
+
+
+//listar arquivos
+$stmt = $conn->query("SELECT id, nome_arquivo FROM arquivos");  
+$arquivos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach($arquivos as $arquivo) {
+    echo "<p><a href='uploads/" . $arquivo['nome_arquivo'] . "'>" . $arquivo['nome_arquivo'] . "</a></p>";
+}
+
+
+
+?>
+
+
